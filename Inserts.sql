bb4fcf5854e8ab8b0b0606ed960c7e023922d23b
trancelestial <bozidar.boba@gmail.com>
2018-12-24 16:59:35 +0100

    Inserts(not finished), EER slight modification

---
+++ Inserts.sql
@@ -0,0 +1,113 @@
+--  
+--korisnicki nalog
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Randall', 'Balestriero','randal@gmail.com','randal');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Romain', 'Cosentino','romain@gmail.com','romain');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Herve', 'Glotin','herve@gmail.com','herve');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Richard', 'Baraniuk','richard@gmail.com','richard');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Spline Filters For End-to-End Deep Learning','http://proceedings.mlr.press/v80/balestriero18a/balestriero18a.pdf','prijavljen',FALSE);
+
+-- The Lingering of Gradients: How to Reuse Gradients Over Time
+
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Zeyuan', 'Allen-Zhu','zhu@gmail.com','alenzhu');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Dadid', 'Simchi-Levi','david@gmail.com','david');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Xinshang', 'Weng','weng@gmail.com','wneg');
+
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('The Lingering of Gradients: How to Reuse Gradients Over Time','http://papers.nips.cc/paper/7400-the-lingering-of-gradients-how-to-reuse-gradients-over-time.pdf','prijavljen',FALSE);
+
+--Quadratic Decomposable Submodular Function Minimization 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Pan', 'Li','pan@gmail.com','pan');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Niao', 'He','niao@gmail.com','niao');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Olgica', 'Milenkovic','olgica@gmail.com','Milenkovic');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Quadratic Decomposable Submodular Function Minimization','http://papers.nips.cc/paper/7383-quadratic-decomposable-submodular-function-minimization.pdf','prijavljen',FALSE);
+
+--Amortized Inference Regularization 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Riu', 'Shu','riu@gmail.com','riushu');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Stefano', 'Ermon','stefanoe@gmail.com','stefanoe');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Amortized Inference Regularization','http://papers.nips.cc/paper/7692-amortized-inference-regularization.pdf','prijavljen',FALSE);
+
+--Leveraging the Exact Likelihood of Deep Latent Variable Models 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Pieree-Alexandre', 'Mattei','pierrem@gmail.com','pierrem');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Jes', 'Frellsen','jes@gmail.com','jesfrel');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Leveraging the Exact Likelihood of Deep Latent Variable Models','http://papers.nips.cc/paper/7642-leveraging-the-exact-likelihood-of-deep-latent-variable-models.pdf','prijavljen',FALSE);
+
+--Invertibility of Convolutional Generative Networks from Partial Measurements 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Fengchang', 'Ma','ma@gmail.com','mafeng');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Ulas', 'Ayaz','ulas@gmail.com','ulasa');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Sertac', 'Karaman','sertac@gmail.com','sertac');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Invertibility of Convolutional Generative Networks from Partial Measurements','http://papers.nips.cc/paper/8171-invertibility-of-convolutional-generative-networks-from-partial-measurements.pdf','prijavljen',FALSE);
+
+--Glow: Generative Flow with Invertible 1x1 Convolutions 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Diederik', 'Kingman','diederik@gmail.com','diederik');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Glow: Generative Flow with Invertible 1x1 Convolutions ','http://papers.nips.cc/paper/8224-glow-generative-flow-with-invertible-1x1-convolutions.pdf','prijavljen',FALSE);
+
+--Multimodal Generative Models for Scalable Weakly-Supervised Learning 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Mike', 'Vu','mikevu@gmail.com','mikevu');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Noah', 'Goodman','noahg@gmail.com','noahg');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Multimodal Generative Models for Scalable Weakly-Supervised Learning','http://papers.nips.cc/paper/7801-multimodal-generative-models-for-scalable-weakly-supervised-learning.pdf','prijavljen',FALSE);
+
+--Adversarial Scene Editing: Automatic Object Removal from Weak Supervision 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Mario', 'Fritz','mariof@gmail.com','mariof');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Rakshith', 'Shetty','shetty@gmail.com','shetty');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Adversarial Scene Editing: Automatic Object Removal from Weak Supervision','http://papers.nips.cc/paper/7997-adversarial-scene-editing-automatic-object-removal-from-weak-supervision.pdf','prijavljen',FALSE);
+
+--Deep Neural Networks with Box Convolutions 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Egor', 'Burkov','egorb@gmail.com','egorb');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Deep Neural Networks with Box Convolutions','http://papers.nips.cc/paper/7859-deep-neural-networks-with-box-convolutions.pdf','prijavljen',FALSE);
+
+--Graphical Generative Adversarial Networks 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Bo', 'Zhang','zhang@gmail.com','zhangbo');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Max', 'Welling','@gmail.com','');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Graphical Generative Adversarial Networks','http://papers.nips.cc/paper/7846-graphical-generative-adversarial-networks.pdf','prijavljen',FALSE);
+
+--A Convex Duality Framework for GANs 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Farzan', 'Farnia','farnia@gmail.com','farnia');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('David', 'Tse','tsed@gmail.com','tsed');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('A Convex Duality Framework for GANs ','http://papers.nips.cc/paper/7771-a-convex-duality-framework-for-gans.pdf','peijavljen',FALSE);
+
+--Banach Wasserstein GAN 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Jonas', 'Adler','adlerj@gmail.com','');
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Sebastian', 'Junz','junz@gmail.com','sebjunz');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Banach Wasserstein GAN','http://papers.nips.cc/paper/7909-banach-wasserstein-gan.pdf','prijavljen',FALSE);
+
+--Generalizing Point Embeddings using the Wasserstein Space of Elliptical Distributions 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Boris', 'Muzellec','borism@gmail.com','borism');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Generalizing Point Embeddings using the Wasserstein Space of Elliptical Distributions','http://papers.nips.cc/paper/8226-generalizing-point-embeddings-using-the-wasserstein-space-of-elliptical-distributions.pdf','prijavljen',FALSE);
+
+--PointCNN: Convolution On X-Transformed Points 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Riu', 'Bu','riu@gmail.com','buriu');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('PointCNN: Convolution On X-Transformed Points','http://papers.nips.cc/paper/7362-pointcnn-convolution-on-x-transformed-points.pdf','prijavljen',FALSE);
+
+--Moonshine: Distilling with Cheap Convolutions 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Gavin', 'Grey','gaving@gmail.com','gaving');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Moonshine: Distilling with Cheap Convolutions','http://papers.nips.cc/paper/7553-moonshine-distilling-with-cheap-convolutions.pdf','prijavljen',FALSE);
+
+--SplineNets: Continuous Neural Decision Graphs 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Cem', 'Keskin','keskin@gmail.com','keskin');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('SplineNets: Continuous Neural Decision Graphs','http://papers.nips.cc/paper/7470-splinenets-continuous-neural-decision-graphs.pdf','prijavljen',FALSE);
+
+--Scalable methods for 8-bit training of neural networks 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Ron', 'Banner','banner@gmail.com','banner');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Scalable methods for 8-bit training of neural networks','http://papers.nips.cc/paper/7761-scalable-methods-for-8-bit-training-of-neural-networks.pdf','prijavljen',FALSE);
+
+--Collaborative Learning for Deep Neural Networks 
+insert into KorisnickiNalog (ime,prezime,email,sifra) values ('Wei', 'Chai','chai@gmail.com','chaiw');
+--rad
+insert into Rad(naslov,pdfStorageLinkId,status,objavljen) values ('Collaborative Learning for Deep Neural Networks','http://papers.nips.cc/paper/7454-collaborative-learning-for-deep-neural-networks.pdf','prijavljen',FALSE);
